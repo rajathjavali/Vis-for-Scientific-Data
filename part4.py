9089ee1e34f8b71f8e0f3ed15c3af47c012308bb
Rajath P Javali <rajathjavali@gmail.com>
2019-01-30 10:42:06 -0700

    modifying part4.py to include all slices of brain mri

--- part4.py
+++ part4.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 
 def show_slices(slices):
 	plt.suptitle("Center slices for EPI image")
-	for _slice in slices:
-		plt.imshow(_slice.T, cmap="gray", origin="lower")
+	fig, axes = plt.subplots(1, len(slices))
+	for i, slice in enumerate(slices):
+		axes[i].imshow(slice.T, cmap="gray", origin="lower")
 	# plt.show()
 	plt.savefig("brain.png")
 
 
-img_load = nib.load('data\\T2.nii')
+img_load = nib.load('./data/T2.nii')
 img_data = img_load.get_fdata()
 img_slice1 = img_data[:, 100, :]
-img_slice2 = img_data[100:, :, :]
+img_slice2 = img_data[100, :, :]
 img_slice3 = img_data[:, :, 100]
 show_slices([img_slice1, img_slice2, img_slice3])
