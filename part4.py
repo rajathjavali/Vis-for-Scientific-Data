1fdeac9bb72fbbb1e31a01d3f09773453dc26458
Rajath P Javali <rajathjavali@gmail.com>
2019-01-30 10:36:43 -0700

    adding dataset

--- part4.py
+++ part4.py
@@ -2,14 +2,17 @@ import nibabel as nib
 import matplotlib.pyplot as plt
 
 
-def show_slices(slice):
-    plt.suptitle("Center slices for EPI image")
-    plt.imshow(slice.T, cmap="gray", origin="lower")
-    # plt.show()
-    plt.savefig("brain.png")
+def show_slices(slices):
+	plt.suptitle("Center slices for EPI image")
+	for _slice in slices:
+		plt.imshow(_slice.T, cmap="gray", origin="lower")
+	# plt.show()
+	plt.savefig("brain.png")
 
 
-img_load = nib.load('data\T2.nii')
+img_load = nib.load('data\\T2.nii')
 img_data = img_load.get_fdata()
-img_slice = img_data[:, 160, :]
-show_slices(img_slice)
+img_slice1 = img_data[:, 100, :]
+img_slice2 = img_data[100:, :, :]
+img_slice3 = img_data[:, :, 100]
+show_slices([img_slice1, img_slice2, img_slice3])
