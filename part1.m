c8498aa24d52661eeb1021a81cfcea413f7e0c61
Rajath Javali <rajathjavali@gmail.com>
2019-01-31 22:40:44 -0700

    adding assign doc file

--- part1.m
+++ part1.m
@@ -1,11 +1,13 @@
 array1 = 1:1:200;
 disp(array1)
+figure(1)
 boxplot(array1)
 ylabel("Values 1-200")
 title('Viz 1 - BoxPlot')
 
 array2 = rand(1,10000);
 disp(array2)
+figure(2)
 histogram(array2, 20)
 xlabel('Bins (Automatic Binning)')
 ylabel('Random Values')
@@ -17,6 +19,7 @@ r = (b-a).*rand(1, 100) + a;
 array = 1:1:100;
 f = fopen('guassian_matlab.bin', 'w');
 fwrite(f, r, 'float');
+figure(3)
 plot(array,r)
 xlabel('Instances 1-100')
 ylabel('Random numbers - Guassian Distribution')
@@ -26,6 +29,7 @@ fclose(f);
 
 input_file = fopen('guassian_matlab.bin');
 r = fread(input_file, 'float');
+figure(4)
 histogram(r, [0,14,28,42,56,70,84,100])
 xticks([0,14,28,42,56,70,84,100])
 ylabel('Frequency')
